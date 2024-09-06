e5e6239857c210b7d2b6156e5cfa00090995d65e
still-nikk <nmaheshwari2904@gmail.com>
2024-09-06 22:40:42 +0530

    tried to fix the background not displaying on deployment

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,70 +1,11 @@
-// import { benefits } from "../constants";
-// import Heading from "./Heading";
-// import Section from "./Section";
-// import Arrow from "../assets/svg/Arrow";
-// import { GradientLight } from "./design/Benefits";
-// import ClipPath from "../assets/svg/ClipPath";
-
-// const Benefits = () => {
-//   return (
-//     <Section crosses id="features">
-//       <div className="container relative z-2">
-//         <Heading
-//           className="md:max-w-md lg:max-w-2xl"
-//           title="Projects I've Worked On."
-//         />
-
-//         <div className="flex flex-wrap gap-10 mb-10">
-//           {benefits.map((item) => (
-//             <div
-//               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-//               style={{
-//                 backgroundImage: `url(${item.backgroundUrl})`,
-//               }}
-//               key={item.id}
-//             >
-//               <div className="relative z-2 flex flex-col h-[22rem] p-[2.4rem] pointer-events-none">
-//                 <h5 className="h5 mb-5">{item.title}</h5>
-//                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-
-//               </div>
-
-//               {/* {item.light && <GradientLight />} */}
-
-//               <div className=" relative z-30 mb-5 mx-5 flex justify-between items-center mt-auto">
-//                   <img
-//                     src={item.iconUrl}
-//                     width={48}
-//                     height={48}
-//                     alt={item.title}
-//                   />
-//                   <div className=" flex justify-center items-center hover:scale-[1.05] transition-transform">
-
-//                   <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider cursor-pointer "
-//                   onClick={() => window.open(item.gitURL, '_blank', 'noopener,noreferrer')}>
-//                     Explore more
-//                   </p>
-//                   <Arrow />
-//                     </div>
-//                 </div>
-
-//               <ClipPath />
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </Section>
-//   );
-// };
-
-// export default Benefits;
-
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
+import card7 from '../assets/benefits/card-1.svg';
+
 
 const Benefits = () => {
   return (
@@ -80,7 +21,7 @@ const Benefits = () => {
             <div
               className="flex justify-between flex-col relative p-5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
+                backgroundImage: `url(${card7})`,
               }}
               key={item.id}
             >
