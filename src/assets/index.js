77cc5759319ffa0d9ac343475ed53357f9042344
still-nikk <nmaheshwari2904@gmail.com>
2024-09-06 22:30:56 +0530

    tried altering the projects component

--- src/assets/index.js
+++ src/assets/index.js
@@ -62,7 +62,7 @@ import notification3 from "./notification/image-3.png";
 import notification4 from "./notification/image-4.png";
 
 
-import benefitCard1 from "./benefits/card-1.svg";
+import benefitCard1 from "./benefits/card-7.svg";
 import benefitCard2 from "./benefits/card-2.svg";
 import benefitCard3 from "./benefits/card-3.svg";
 import benefitCard4 from "./benefits/card-4.svg";
